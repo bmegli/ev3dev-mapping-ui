3b5bde26c8db0e8bbe3ca73348306398df3aaa58
Bartosz Meglicki <bmegli@op.pl>
2016-09-30 14:33:55 +0200

    Rename odometry to dead-reconning, create odometry
    Also simplify class hierarchy.
    
    Got rid off:
    - MotionModels class hierarchy
    - DifferentialDrive class hierarchy
    
    It's not necessary to complicate at this point. This can be reintrocuded
    as needed.
    
    For now Odometry has binary compatiblity with DeadReconnning so that
    replays can be shared to rebuild with odoemtry model from dead reconning
    record. This is not guaranteed to be compatible in the future.

---
+++ Assets/Scripts/Robot/Physics.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Physics : MonoBehaviour
+{
+	public float wheelDiameterMm=43.2f;
+	public float wheelbaseMm=250.0f;
+	public int encoderCountsPerRotation=360;
+	public int maxEncoderCountsPerSecond=1000;
+	public bool reverseMotorPolarity=false;
+
+	public float MMPerCount()
+	{
+		return Mathf.PI * wheelDiameterMm / encoderCountsPerRotation;
+	}
+	public float CountsPerMM()
+	{
+		return encoderCountsPerRotation / (Mathf.PI * wheelDiameterMm);
+	}
+		
+	public Physics DeepCopy()
+	{
+		Physics other = (Physics) this.MemberwiseClone();
+		return other;
+	}
+
+}

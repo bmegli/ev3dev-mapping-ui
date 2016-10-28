221020d8374dabcbbea8064fc3a8b80d81d246eb
Bartosz Meglicki <bmegli@op.pl>
2016-10-28 15:16:55 +0200

    work with reprocesssed communication

--- Assets/Scripts/DeadReconning/DeadReconningPacket.cs
+++ Assets/Scripts/DeadReconning/DeadReconningPacket.cs
@@ -37,25 +37,19 @@ public class DeadReconningPacket : IDatagram
 		timestamp_us = (ulong)IPAddress.NetworkToHostOrder(reader.ReadInt64());
 		position_left = IPAddress.NetworkToHostOrder(reader.ReadInt32());
 		position_right = IPAddress.NetworkToHostOrder(reader.ReadInt32());
-		speed_left = IPAddress.NetworkToHostOrder(reader.ReadInt32());
-		speed_right = IPAddress.NetworkToHostOrder(reader.ReadInt32());
 		heading = IPAddress.NetworkToHostOrder(reader.ReadInt16());
-		angular_speed = IPAddress.NetworkToHostOrder(reader.ReadInt16());
 	}
 	public void ToBinary(System.IO.BinaryWriter writer)
 	{
 		writer.Write(IPAddress.HostToNetworkOrder((long)timestamp_us));
 		writer.Write(IPAddress.HostToNetworkOrder(position_left));
 		writer.Write(IPAddress.HostToNetworkOrder(position_right));
-		writer.Write(IPAddress.HostToNetworkOrder(speed_left));
-		writer.Write(IPAddress.HostToNetworkOrder(speed_right));
 		writer.Write(IPAddress.HostToNetworkOrder(heading));
-		writer.Write(IPAddress.HostToNetworkOrder(angular_speed));
 	}
 
 	public int BinarySize()
 	{
-		return 28; // 8+ 4*4 +2*2
+		return 18; // 8+ 2*4 +2
 	}
 
 	public void CloneFrom(DeadReconningPacket p)

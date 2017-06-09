334573b3cdbe7a40bb4a490b8671f376c4458b1c
Bartosz Meglicki <bmegli@op.pl>
2017-06-09 23:48:41 +0200

    Features component, architecture for feature extraction
    adds Features component (in Unity, not ev3dev-mapping sense) with
    arhitecture designed for feature extraction
    
    When Laser component collects 360 degree scan it pushes it into Features
    component scans queue (thread safely) and signals Features extraction
    thread to wake up.
    
    Features extraction thread wakes up, grabs the scans (thread safely)
    processes them.
    
    The remeaining part to write is the feature extraction itself, thread
    safe push of results to Unity thread and the visualizaiton of features.

---
+++ Assets/Scripts/Features/Features.cs
@@ -0,0 +1,131 @@
+﻿/*
+ * Copyright (C) 2017 Bartosz Meglicki <meglickib@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Scan360
+{
+	private Vector3[] readings=new Vector3[360];
+	private bool[] invalid_data=new bool[360];
+
+	public Scan360(Vector3[] readings, bool[] invalid_data)
+	{
+		Array.Copy(readings, this.readings, 360);
+		Array.Copy(invalid_data, this.invalid_data, 360);
+	}
+}
+
+public class Features : MonoBehaviour
+{	
+	private bool Run { get; set; }
+	private Thread scanThread;
+	private AutoResetEvent scanWaitEvent = new AutoResetEvent(false);
+
+	private Queue<Scan360> waitingScans=new Queue<Scan360>();
+	private object waitingScansLock = new object();
+
+
+	void Awake()
+	{		
+	}
+	void Start ()
+	{
+		print(name + " - starting extraction thread");
+		StartScanThread();
+	}	
+	void Update ()
+	{
+		// this is the place to retrieve feature extraction in a thread safe manner and visualize it
+	}
+	void OnDestroy()
+	{
+		print(name + " - stopping extraction thread");
+		StopScanThread();
+	}
+
+	void StartScanThread()
+	{
+		if (scanThread != null)
+		{
+			Debug.LogError(name + " - scan thread is already running");
+			throw new InvalidOperationException(name + " - scan thread is already running");
+		}
+		scanThread = new Thread(new ThreadStart(ScanThreadMain));
+		Run = true;
+		scanThread.Start();
+
+	}
+
+	void StopScanThread()
+	{
+		Run = false;
+		scanWaitEvent.Set();
+	}
+
+	#region Thread Safe Functions
+
+	public void PutScanThreadSafe(Vector3[] readings, bool[] invalid_data)
+	{
+		//to consider - circular buffer of scans and swap buffers instead of copy
+		//copy is ok for now
+
+		Scan360 scan = new Scan360(readings, invalid_data);
+
+		lock(waitingScansLock)
+		{
+			waitingScans.Enqueue(scan);
+		}
+
+		scanWaitEvent.Set();
+	}
+		
+	#endregion
+
+	#region Feature Extraction Thread
+
+	void ScanThreadMain()
+	{
+		Queue<Scan360> scans = new Queue<Scan360>();
+		Queue<Scan360> temp = new Queue<Scan360>();
+
+		while (scanWaitEvent.WaitOne())
+		{
+			if (!Run)
+				break;
+
+			lock(waitingScansLock)
+			{
+				temp = waitingScans;
+				waitingScans = scans;
+				scans = temp;
+			}
+
+			while (scans.Count > 0)
+				ProcessScan(scans.Dequeue());
+		}
+
+		print("Features - finished");
+	}
+
+	void ProcessScan(Scan360 scan)
+	{
+		// this is the place to make the actual feature extraction
+
+		// here is the place to push results to Unity in a thread safe manner
+	}
+
+	#endregion
+}

0d67a631a947521b2d475fd5b15620377dd1c5be
Bartosz Meglicki <bmegli@op.pl>
2018-06-06 17:59:56 +0200

    basic support for 3D dead reconning
    - packet with Quaternion data
    - DeadReconning3D receiving quaterion data and UI
    - setting rotation to received data

---
+++ Assets/Scripts/DeadReconning/DeadReconning3DPacket.cs
@@ -0,0 +1,106 @@
+﻿/*
+ * Copyright (C) 2018 Bartosz Meglicki <meglickib@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ * This program is distributed "as is" WITHOUT ANY WARRANTY of any
+ * kind, whether express or implied; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+using System.Net;
+using System;
+
+public class DeadReconning3DPacket : IDatagram
+{
+	public ulong timestamp_us;
+	public int position_left;
+	public int position_right;
+	//in degrees, consider array or vector4
+	public float quat_w;
+	public float quat_x; 
+	public float quat_y;
+	public float quat_z;
+
+	/*
+	public float HeadingInDegrees
+	{
+		get{return euler_y;}
+	}
+	*/
+
+	public override string ToString()
+	{
+		return string.Format("[timestamp={0} pl={1} pr={2} w={3} x={4} y={5} z={6}]", timestamp_us, position_left, position_right, quat_w, quat_x, quat_y, quat_z);
+	}
+
+	public void FromBinary(System.IO.BinaryReader reader)
+	{
+		byte[] data;
+		timestamp_us = (ulong)IPAddress.NetworkToHostOrder(reader.ReadInt64());
+		position_left = IPAddress.NetworkToHostOrder(reader.ReadInt32());
+		position_right = IPAddress.NetworkToHostOrder(reader.ReadInt32());
+
+		data = reader.ReadBytes(4);
+		Array.Reverse (data);
+		quat_w=BitConverter.ToSingle (data, 0);
+
+		data = reader.ReadBytes(4);
+		Array.Reverse (data);
+		quat_x=BitConverter.ToSingle (data, 0);
+
+		data = reader.ReadBytes(4);
+		Array.Reverse (data);
+		quat_y=BitConverter.ToSingle (data, 0);
+
+		data = reader.ReadBytes(4);
+		Array.Reverse (data);
+		quat_z=BitConverter.ToSingle (data, 0);
+	}
+	public void ToBinary(System.IO.BinaryWriter writer)
+	{
+		byte[] data;
+		writer.Write(IPAddress.HostToNetworkOrder((long)timestamp_us));
+		writer.Write(IPAddress.HostToNetworkOrder(position_left));
+		writer.Write(IPAddress.HostToNetworkOrder(position_right));
+
+		data = BitConverter.GetBytes (quat_w);
+		Array.Reverse (data);
+		writer.Write(data);
+
+		data = BitConverter.GetBytes (quat_x);
+		Array.Reverse (data);
+		writer.Write(data);
+
+		data = BitConverter.GetBytes (quat_y);
+		Array.Reverse (data);
+		writer.Write(data);
+
+		data = BitConverter.GetBytes (quat_z);
+		Array.Reverse (data);
+		writer.Write(data);
+	}
+
+	public int BinarySize()
+	{
+		return 32; // 8 + 2*4 + 4*4
+	}
+
+	public void CloneFrom(DeadReconning3DPacket p)
+	{
+		timestamp_us = p.timestamp_us;
+		position_left = p.position_left;
+		position_right = p.position_right;
+		quat_w = p.quat_w;
+		quat_x = p.quat_x;
+		quat_y = p.quat_y;
+		quat_z = p.quat_z;
+	}
+
+	public ulong GetTimestampUs()
+	{
+		return timestamp_us;
+	}
+}

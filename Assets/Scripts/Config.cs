e35daadedccfdf88b6f92442c6e6f56d4d113a05
Bartosz Meglicki <bmegli@op.pl>
2018-08-01 07:38:35 +0200

    Application.persistentDataPath for storage on non-standalone platforms
    Standalone platforms use project folder for simplicity for storage.
    This is not possible for platforms like Android so use persistendDataPath.

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -1,4 +1,4 @@
-﻿/*
+﻿﻿/*
  * Copyright (C) 2016 Bartosz Meglicki <meglickib@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -15,43 +15,47 @@ using System.Collections;
 
 namespace Ev3devMapping
 {
-
-public class Config
-{
-	public const string MAPS_DIRECTORY="PLY";
-	public const string DUMPS_DIRECTORY="UDP";
-	public const string SNAPSHOTS_DIRECTORY="SNAPSHOT";
-
-	public static string DumpPath(string dump_dir, string dump_subdir)
+	public class Config
 	{
-		return DUMPS_DIRECTORY + "/" + dump_dir + "/" + dump_subdir;
-	}
-		
-	public static string DumpPath(string dump_dir, string dump_subdir, string dump_file)
-	{
-		return DUMPS_DIRECTORY + "/" + dump_dir + "/" + dump_subdir + "/" + dump_file + ".bin";
-	}
-		
-	public static string MapPath(string map_dir, string map_subdir)
-	{
-		return MAPS_DIRECTORY + "/" + map_dir + "/" + map_subdir;
-	}
-	public static string MapPath(string map_dir, string map_subdir, string map_file)
-	{
-		return MAPS_DIRECTORY + "/" + map_dir + "/" + map_subdir + "/" + map_file + ".ply";
-	}
+		#if UNITY_STANDALONE
+		public static string PREFIX =  "";
+		#else
+		public static string PREFIX = Application.persistentDataPath + "/";
+		#endif
+		public const string MAPS_DIRECTORY="PLY";
+		public const string DUMPS_DIRECTORY="UDP";
+		public const string SNAPSHOTS_DIRECTORY="SNAPSHOT";
 
-	public static string SnapshotPath(string snap_dir, string snap_subdir)
-	{
-		return SNAPSHOTS_DIRECTORY + "/" + snap_dir + "/" + snap_subdir;
+		public static string DumpPath(string dump_dir, string dump_subdir)
+		{
+			return PREFIX + DUMPS_DIRECTORY + "/" + dump_dir + "/" + dump_subdir;
+		}
+
+		public static string DumpPath(string dump_dir, string dump_subdir, string dump_file)
+		{
+			return PREFIX + DUMPS_DIRECTORY + "/" + dump_dir + "/" + dump_subdir + "/" + dump_file + ".bin";
+		}
+
+		public static string MapPath(string map_dir, string map_subdir)
+		{
+			return PREFIX + MAPS_DIRECTORY + "/" + map_dir + "/" + map_subdir;
+		}
+		public static string MapPath(string map_dir, string map_subdir, string map_file)
+		{
+			return PREFIX + MAPS_DIRECTORY + "/" + map_dir + "/" + map_subdir + "/" + map_file + ".ply";
+		}
+
+		public static string SnapshotPath(string snap_dir, string snap_subdir)
+		{
+			return PREFIX + SNAPSHOTS_DIRECTORY + "/" + snap_dir + "/" + snap_subdir;
+		}
+
+		public static string SnapshotPath(string snap_dir, string snap_subdir, string snap_file)
+		{
+			return PREFIX + SNAPSHOTS_DIRECTORY + "/" + snap_dir + "/" + snap_subdir + "/" + snap_file + ".csv";
+		}
+
+
 	}
-
-	public static string SnapshotPath(string snap_dir, string snap_subdir, string snap_file)
-	{
-		return SNAPSHOTS_DIRECTORY + "/" + snap_dir + "/" + snap_subdir + "/" + snap_file + ".csv";
-	}
-
-
-}
 
 } //namespace

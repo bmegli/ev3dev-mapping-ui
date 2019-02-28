a5098dfb61452c524e4e3daf45f2c44ab5d59d8b
Bartosz Meglicki <bmegli@op.pl>
2019-02-28 21:14:04 +0100

    rename DragHandler to DragMoveScaleHandler
    this reflects the intent of handler

---
+++ Assets/Scripts/Camera/DragMoveScaleHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragMoveScaleHandler : MonoBehaviour, IDragHandler
+{
+	public void OnDrag(PointerEventData eventData)
+	{
+		if (eventData.button == PointerEventData.InputButton.Left)
+			transform.position += (Vector3)(eventData.delta);
+		else if (eventData.button == PointerEventData.InputButton.Right)
+		{
+			transform.localScale += new Vector3 (0.01f, 0.01f, 0f) * eventData.delta.y;
+			if (transform.localScale.x < 0.2)
+				transform.localScale = new Vector3 (0.2f, 0.2f, 1f);
+		}
+	}
+}

5a51c16bbd6d9412f49476f0742d2b0287d312ec
Bartosz Meglicki <bmegli@op.pl>
2019-02-28 18:35:02 +0100

    low latency hardware video decoding plugin
    through NHVD

---
+++ Assets/Scripts/Camera/NHVD.cs
@@ -0,0 +1,96 @@
+﻿/*
+ * Unity Network Hardware Video Decoder
+ * 
+ * Copyright 2019 (C) Bartosz Meglicki <meglickib@gmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+
+public class NHVD
+{
+	[System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential)]
+	public struct nhvd_hw_config
+	{
+		[System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.LPStr)]
+		public string hardware;
+
+		[System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.LPStr)]
+		public string codec;
+
+		[System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.LPStr)]
+		public string device;
+
+		[System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.LPStr)]
+		public string pixel_format;
+	}
+
+	[System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential)]
+	public struct nhvd_net_config
+	{
+		[System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.LPStr)]
+		public string ip;
+		public ushort port;
+		public int timeout_ms;
+	}
+
+	[System.Runtime.InteropServices.StructLayoutAttribute(System.Runtime.InteropServices.LayoutKind.Sequential)]
+	public struct nhvd_frame
+	{
+		public int width;
+		public int height;
+		public int format;
+
+		/// uint8t *[3]
+		[System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.ByValArray, SizeConst=3, ArraySubType=System.Runtime.InteropServices.UnmanagedType.SysUInt)]
+		public System.IntPtr[] data;
+
+		/// int[3]
+		[System.Runtime.InteropServices.MarshalAsAttribute(System.Runtime.InteropServices.UnmanagedType.ByValArray, SizeConst=3, ArraySubType=System.Runtime.InteropServices.UnmanagedType.I4)]
+		public int[] linesize;
+	}
+
+	/// Return Type: nhvd*
+	///net_config: nhvd_net_config*
+	///hw_config: nhvd_hw_config*
+	#if (UNITY_IPHONE || UNITY_WEBGL) && !UNITY_EDITOR
+	[DllImport ("__Internal")]
+	#else
+	[DllImport ("nhvd")]
+	#endif
+	public static extern System.IntPtr nhvd_init(ref nhvd_net_config net_config, ref nhvd_hw_config hw_config) ;
+
+	/// Return Type: void
+	///n: nhvd *
+	#if (UNITY_IPHONE || UNITY_WEBGL) && !UNITY_EDITOR
+	[DllImport ("__Internal")]
+	#else
+	[DllImport ("nhvd")]
+	#endif
+	public static extern void nhvd_close(System.IntPtr n) ;
+
+	/// Return Type: int
+	///n: void*
+	///frame: nhvd_frame*
+	#if (UNITY_IPHONE || UNITY_WEBGL) && !UNITY_EDITOR
+	[DllImport ("__Internal")]
+	#else
+	[DllImport ("nhvd")]
+	#endif
+	public static extern int nhvd_get_frame_begin(System.IntPtr n, ref nhvd_frame frame);
+
+	/// Return Type: int
+	///n: nhvd *
+	#if (UNITY_IPHONE || UNITY_WEBGL) && !UNITY_EDITOR
+	[DllImport ("__Internal")]
+	#else
+	[DllImport ("nhvd")]
+	#endif
+	public static extern int nhvd_get_frame_end(System.IntPtr n) ;
+}
---
+++ Assets/Scripts/Camera/VideoRenderer.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * Unity Network Hardware Video Decoder
+ * 
+ * Copyright 2019 (C) Bartosz Meglicki <meglickib@gmail.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+using System;
+using UnityEngine;
+
+public class VideoRenderer : MonoBehaviour
+{
+	public string device = "/dev/dri/renderD128";
+	public string ip = "";
+	public ushort port = 9766;
+
+	private IntPtr nhvd;
+	private NHVD.nhvd_frame frame = new NHVD.nhvd_frame{ data=new System.IntPtr[3], linesize=new int[3] };
+	private Texture2D videoTexture;
+
+	void Awake()
+	{
+		NHVD.nhvd_hw_config hw_config = new NHVD.nhvd_hw_config{hardware="vaapi", codec="h264", device=this.device, pixel_format="bgr0"};
+		NHVD.nhvd_net_config net_config = new NHVD.nhvd_net_config{ip=this.ip, port=this.port, timeout_ms=500 };
+
+		nhvd=NHVD.nhvd_init (ref net_config, ref hw_config);
+
+		if (nhvd == IntPtr.Zero)
+		{
+			Debug.Log ("failed to initialize NHVD");
+			gameObject.SetActive (false);
+		}
+
+		//flip the texture mapping upside down
+		Vector2[] uv = GetComponent<MeshFilter>().mesh.uv;
+		for (int i = 0; i < uv.Length; ++i)
+			uv [i][1] = -uv [i][1];
+		GetComponent<MeshFilter> ().mesh.uv = uv;
+	}
+	void OnDestroy()
+	{
+		NHVD.nhvd_close (nhvd);
+	}
+
+	private void AdaptTexture()
+	{
+		if(videoTexture== null || videoTexture.width != frame.width || videoTexture.height != frame.height)
+		{
+			videoTexture = new Texture2D (frame.width, frame.height, TextureFormat.BGRA32, false);
+			GetComponent<Renderer> ().material.mainTexture = videoTexture;
+		}
+	}
+
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+		if (NHVD.nhvd_get_frame_begin(nhvd, ref frame) == 0)
+		{
+			AdaptTexture ();
+			videoTexture.LoadRawTextureData (frame.data[0], frame.width*frame.height*4);
+			videoTexture.Apply (false);
+		}
+
+		if (NHVD.nhvd_get_frame_end (nhvd) != 0)
+			Debug.LogWarning ("Failed to get NHVD frame data");
+	}
+}

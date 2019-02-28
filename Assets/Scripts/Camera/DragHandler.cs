407dc021e172e747d97dab7b4b690d0d2d5fbf59
Bartosz Meglicki <bmegli@op.pl>
2019-02-28 20:44:17 +0100

    make the camera view draggable

---
+++ Assets/Scripts/Camera/DragHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IDragHandler
+{
+	public void OnDrag(PointerEventData eventData)
+	{
+		transform.position += (Vector3)(eventData.delta);
+	}
+}
